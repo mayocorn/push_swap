8a862020e28444a270b92666a4ebafb4156531d5
mayocorn <stsunoda@student.42tokyo.jp>
2022-06-21 08:44:28 +0900

    feat(duplicate_int_array.{c.h}): add new file

---
+++ include/duplicate_int_array.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   duplicate_int_array.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mayocorn <twitter@mayocornsuki>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/20 20:29:57 by mayocorn          #+#    #+#             */
+/*   Updated: 2022/06/20 20:38:09 by mayocorn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DUPLICATE_INT_ARRAY_H
+# define DUPLICATE_INT_ARRAY_H
+# include <stdlib.h>
+# include "../include/utils.h"
+# include "../include/libft.h"
+
+int	*duplicate_int_array(size_t size, int *src);
+#endif
