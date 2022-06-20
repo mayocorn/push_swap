ac73b51f72d6f1fd905e71bea35ddcee48f610c3
mayocorn <stsunoda@student.42tokyo.jp>
2022-06-21 08:46:52 +0900

    fix(mergesort.c): swap line 44, 45

--- src/mergesort.c
+++ src/mergesort.c
@@ -6,7 +6,7 @@
 /*   By: mayocorn <twitter@mayocornsuki>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 15:23:31 by mayocorn          #+#    #+#             */
-/*   Updated: 2022/06/20 20:04:38 by mayocorn         ###   ########.fr       */
+/*   Updated: 2022/06/20 20:53:28 by mayocorn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,8 +41,8 @@ static void	merge_array(size_t a_size, size_t b_size, \
 	size_t	array_size;
 
 	array = array_a;
-	tmp = malloc_and_checknull(array_size * sizeof(int));
 	array_size = a_size + b_size;
+	tmp = malloc_and_checknull(array_size * sizeof(int));
 	while (a_size || b_size)
 	{
 		if (b_size == 0 || (a_size != 0 && *array_a < *array_b))
@@ -58,5 +58,5 @@ static void	merge_array(size_t a_size, size_t b_size, \
 	}
 	tmp -= array_size;
 	ft_memmove(array, tmp, array_size * sizeof(int));
-	free_and_setnull(&tmp);
+	free_and_setnull((void**)&tmp);
 }
