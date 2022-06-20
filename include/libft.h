1eca25106348b4d9f052107102992c96df64445f
mayocorn <stsunoda@student.42tokyo.jp>
2022-06-20 20:10:30 +0900

    feat(utils.{c/h}): change function/file name

---
+++ include/libft.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mayocorn <twitter@mayocornsuki>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/20 18:10:13 by mayocorn          #+#    #+#             */
+/*   Updated: 2022/06/20 19:00:03 by mayocorn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <stdlib.h>
+
+int		ft_isdigit(int c);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+#endif
---
+++ src/utils.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mayocorn <twitter@mayocornsuki>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/16 17:38:52 by mayocorn          #+#    #+#             */
+/*   Updated: 2022/06/20 19:03:14 by mayocorn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/utils.h"
+
+int	ft_min(int a, int b)
+{
+	if (a < b)
+		return (a);
+	else
+		return (b);
+}
+
+int	ft_max(int a, int b)
+{
+	if (a > b)
+		return (a);
+	else
+		return (b);
+}
+
+void	*malloc_and_checknull(size_t size)
+{
+	void	*res;
+
+	res = malloc(size);
+	if (res == NULL)
+		ft_exit(1);
+	return (res);
+}
+
+void	free_and_setnull(void **ptr)
+{
+	free(*ptr);
+	*ptr = NULL;
+}
+
+void	ft_exit(int status)
+{
+	if (status == 0)
+		exit(0);
+	write(2, "ERROR\n", 6);
+	exit(1);
+}
