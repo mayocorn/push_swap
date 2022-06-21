14f9038ec83d66846931fcf1fa432d2ccaadf5bb
mayocorn <stsunoda@student.42tokyo.jp>
2022-06-21 15:39:05 +0900

    feat(array.h, command.h): integrate array.h, add command.h
    combined convert_array_stoi.h  with array.h

--- include/command.h
+++ include/command.h
@@ -1,25 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   array.h                                            :+:      :+:    :+:   */
+/*   command.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mayocorn <twitter@mayocornsuki>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/20 20:29:57 by mayocorn          #+#    #+#             */
-/*   Updated: 2022/06/21 15:21:30 by mayocorn         ###   ########.fr       */
+/*   Created: 2022/06/21 12:31:11 by mayocorn          #+#    #+#             */
+/*   Updated: 2022/06/21 12:40:17 by mayocorn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ARRAY_H
-# define ARRAY_H
-# include <stdlib.h>
-# include <limits.h>
-# include "utils.h"
-# include "libft.h"
-# define POSITIVE 1
-# define NEGATIVE -1
+#ifndef COMMAND_H
+# define COMMAND_H
 
-int	*duplicate_int_array(size_t size, int *src);
-void check_unique(size_t size, int *array);
-int	*convert_array_str_to_int(size_t array_size, char **src);
+typedef enum e_command
+{
+	sa,
+	sb,
+	ss,
+	pa,
+	pb,
+	ra,
+	rb,
+	rr,
+	rra,
+	rrb,
+	rrr
+}	t_command;
+
+typedef struct s_command_node
+{
+	t_command	command;
+	void		*prev;
+	void		*next;
+}	t_command_node;
+
 #endif
