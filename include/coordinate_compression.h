14abff912544f8b4c8bc8f13c20ad2c46027a7aa
mayocorn <stsunoda@student.42tokyo.jp>
2022-06-21 10:20:30 +0900

    feat(coordinate_compression.{c/h}): add file

---
+++ include/coordinate_compression.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   coordinate_compression.h                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mayocorn <twitter@mayocornsuki>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/21 09:20:50 by mayocorn          #+#    #+#             */
+/*   Updated: 2022/06/21 09:28:49 by mayocorn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COORDINATE_COMPRESSION_H
+# define COORDINATE_COMPRESSION_H
+# include <stdlib.h>
+# include "utils.h"
+
+int	*coordinate_compression(size_t size, int *original, int *sorted);
+#endif
---
+++ src/coordinate_compression.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   coordinate_compression.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mayocorn <twitter@mayocornsuki>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/21 09:20:08 by mayocorn          #+#    #+#             */
+/*   Updated: 2022/06/21 09:53:11 by mayocorn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/coordinate_compression.h"
+
+static size_t	get_index(size_t size, int *sorted, int x);
+
+int	*coordinate_compression(size_t size, int *original, int *sorted)
+{
+	int		*res;
+	size_t	i;
+
+	res = malloc_and_checknull(size * sizeof(int));
+	i = 0;
+	while (i < size)
+	{
+		res[i] = get_index(size, sorted, original[i]);
+		i++;
+	}
+	return (res);
+}
+
+static size_t	get_index(size_t size, int *sorted, int x)
+{
+	size_t	begin;
+	size_t	end;
+	size_t	mid;
+
+	begin = 0;
+	end = size;
+	while (end - begin > 1)
+	{
+		mid = (begin + end) / 2;
+		if (sorted[mid] == x)
+			return (mid);
+		if (sorted[mid] < x)
+			begin = mid + 1;
+		if (sorted[mid] > x)
+			end = mid;
+	}
+	return (begin);
+}
