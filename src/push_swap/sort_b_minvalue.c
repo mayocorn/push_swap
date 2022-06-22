8aa2655669e1c36087efb034b94ce7b1b159aa41
mayocorn <stsunoda@student.42tokyo.jp>
2022-06-22 12:20:20 +0900

    feat(push_swap/): add file

--- src/push_swap/sort_b_minvalue.c
+++ src/push_swap/sort_b_minvalue.c
@@ -1,25 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.c                                        :+:      :+:    :+:   */
+/*   sort_b_minvalue.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mayocorn <twitter@mayocornsuki>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/21 20:45:42 by mayocorn          #+#    #+#             */
-/*   Updated: 2022/06/22 12:15:21 by mayocorn         ###   ########.fr       */
+/*   Created: 2022/06/21 20:45:02 by mayocorn          #+#    #+#             */
+/*   Updated: 2022/06/21 20:45:17 by mayocorn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../include/push_swap.h"
-
-t_deque	*push_swap(size_t size, int *array)
+void	sort_b_minvalue()
 {
-	t_deque	*command_list;
-	t_deque	*element_list;
 
-	command_list = create_deque();
-	element_list = create_deque();
-	push_swap_start();
-	
-	return (command_list);
 }
