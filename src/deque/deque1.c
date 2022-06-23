637284e80267c693513f8e44d40090caea0f4fdd
mayocorn <stsunoda@student.42tokyo.jp>
2022-06-23 20:33:03 +0900

    style(deque1.c): apply norm

--- src/deque/deque1.c
+++ src/deque/deque1.c
@@ -6,7 +6,7 @@
 /*   By: mayocorn <twitter@mayocornsuki>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 15:52:27 by mayocorn          #+#    #+#             */
-/*   Updated: 2022/06/22 09:16:19 by mayocorn         ###   ########.fr       */
+/*   Updated: 2022/06/23 20:31:38 by mayocorn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ t_deque	*create_deque(void)
 	t_deque	*res;
 
 	res = malloc_and_checknull(sizeof(t_deque));
-	res->size = 0;
-	res->front = NULL;
-	res->back = NULL;
+	res -> size = 0;
+	res -> front = NULL;
+	res -> back = NULL;
 	return (res);
 }
 
@@ -27,38 +27,38 @@ void	pushback(t_deque *deque, t_node *ptr)
 {
 	t_node	*prev;
 
-	if (deque->size == 0)
+	if (deque -> size == 0)
 	{
-		deque->front = ptr;
-		deque->back = ptr;
+		deque -> front = ptr;
+		deque -> back = ptr;
 	}
 	else
 	{
 		prev = deque->back;
-		prev->next = ptr;
-		ptr->prev = prev;
-		deque->back = ptr;
+		prev -> next = ptr;
+		ptr -> prev = prev;
+		deque -> back = ptr;
 	}
-	deque->size++;
+	deque -> size++;
 }
 
 void	pushfront(t_deque *deque, t_node *ptr)
 {
 	t_node	*next;
 
-	if (deque->size == 0)
+	if (deque -> size == 0)
 	{
-		deque->front = ptr;
-		deque->back = ptr;
+		deque -> front = ptr;
+		deque -> back = ptr;
 	}
 	else
 	{
-		next = deque->front;
-		next->prev = ptr;
-		ptr->next = next;
-		deque->front = ptr;
+		next = deque -> front;
+		next -> prev = ptr;
+		ptr -> next = next;
+		deque -> front = ptr;
 	}
-	deque->size++;
+	deque -> size++;
 }
 
 t_node	*popback(t_deque *deque)
@@ -66,16 +66,16 @@ t_node	*popback(t_deque *deque)
 	t_node	*res;
 	t_node	*prev;
 
-	res = deque->back;
-	prev = res->prev;
-	deque->back = prev;
+	res = deque -> back;
+	prev = res -> prev;
+	deque -> back = prev;
 	if (prev != NULL)
-		prev->next = NULL;
+		prev -> next = NULL;
 	else
-		deque->front = NULL;
-	res->prev = NULL;
-	deque->size--;
-	return ((t_node*)res);
+		deque -> front = NULL;
+	res -> prev = NULL;
+	deque -> size--;
+	return ((t_node *) res);
 }
 
 t_node	*popfront(t_deque *deque)
@@ -83,14 +83,14 @@ t_node	*popfront(t_deque *deque)
 	t_node	*res;
 	t_node	*next;
 
-	res = deque->front;
-	next = res->next;
-	deque->front = next;
+	res = deque -> front;
+	next = res -> next;
+	deque -> front = next;
 	if (next != NULL)
-		next->prev = NULL;
+		next -> prev = NULL;
 	else
-		deque->back = NULL;
-	res->next = NULL;
-	deque->size--;
-	return ((t_node*)res);
+		deque -> back = NULL;
+	res -> next = NULL;
+	deque -> size--;
+	return ((t_node *)res);
 }
