6cad10ac49dc0d36e642197a3040b3bb862a2a7a
mayocorn <stsunoda@student.42tokyo.jp>
2022-06-23 20:38:08 +0900

    feat(push_swap.c): change variable name & call free func

--- src/push_swap/push_swap.c
+++ src/push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: mayocorn <twitter@mayocornsuki>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 20:45:42 by mayocorn          #+#    #+#             */
-/*   Updated: 2022/06/22 19:10:29 by mayocorn         ###   ########.fr       */
+/*   Updated: 2022/06/23 20:12:24 by mayocorn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,24 @@
 
 static void	create_initial_deque(size_t size, int *array, t_deque *element_list);
 
-t_deque	*push_swap(size_t size, int *array)
+t_deque	*push_swap(size_t size, int **array)
 {
-	t_deque	*command_list;
-	t_deque	*element_list;
+	t_deque	*commands;
+	t_deque	*elements;
 
-	command_list = create_deque();
-	element_list = create_deque();
-	create_initial_deque(size, array, element_list);
-	sort_a_start(element_list, command_list);
-	return (command_list);
+	commands = create_deque();
+	elements = create_deque();
+	create_initial_deque(size, *array, elements);
+	free_and_setnull((void**)array);
+	sort_a_start(elements, commands);
+	return (commands);
 }
 
-static void	create_initial_deque(size_t size, int *array, t_deque *element_list)
+static void	create_initial_deque(size_t size, int *array, t_deque *elements)
 {
 	while(size--)
 	{
-		pushback(element_list, create_number_node(*array));
+		pushback(elements, create_number_node(*array));
 		array++;
 	}
 }
