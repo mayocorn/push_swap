7507eaddfcc8c9072880a00e72f341538afe0ad5
mayocorn <stsunoda@student.42tokyo.jp>
2022-06-24 20:35:23 +0900

    feat(delete_back.c): add delete_back_rb func

--- src/push_swap/delete_back.c
+++ src/push_swap/delete_back.c
@@ -6,25 +6,44 @@
 /*   By: mayocorn <twitter@mayocornsuki>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/24 01:30:40 by mayocorn          #+#    #+#             */
-/*   Updated: 2022/06/24 01:31:30 by mayocorn         ###   ########.fr       */
+/*   Updated: 2022/06/24 18:23:21 by mayocorn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/push_swap.h"
 
-void	delete_back_ra(t_deques *deques)
+void	delete_back_ra(t_div_info *div_info)
 {
 	t_content	content;
 	t_deque		*commands;
 
-	commands = deques -> commands;
+	commands = div_info -> commands;
 	while (commands -> size)
 	{
-		content.command = ((t_node *)(commands -> back))-> content.command;
+		content.command = commands -> back -> content.command;
 		if (content.command == ra)
 		{
 			free(popback(commands));
-			pushfront(deques -> af, popback(deques -> af));
+			pushfront(div_info -> l_q, popback(div_info -> l_q));
+		}
+		else
+			break ;
+	}
+}
+
+void	delete_back_rb(t_div_info *div_info)
+{
+	t_content	content;
+	t_deque		*commands;
+
+	commands = div_info -> commands;
+	while (commands -> size)
+	{
+		content.command = commands -> back -> content.command;
+		if (content.command == rb)
+		{
+			free(popback(commands));
+			pushfront(div_info -> s_q, popback(div_info -> s_q));
 		}
 		else
 			break ;
