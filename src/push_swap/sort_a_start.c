465c8b0401feb57b43fe4cec7d7184c2fffdd522
mayocorn <stsunoda@student.42tokyo.jp>
2022-06-24 14:24:35 +0900

    feat(sort_a_start.c): change to use div_info

--- src/push_swap/sort_a_start.c
+++ src/push_swap/sort_a_start.c
@@ -6,75 +6,89 @@
 /*   By: mayocorn <twitter@mayocornsuki>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/22 15:09:52 by mayocorn          #+#    #+#             */
-/*   Updated: 2022/06/24 01:31:19 by mayocorn         ###   ########.fr       */
+/*   Updated: 2022/06/24 14:23:21 by mayocorn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/push_swap.h"
 
-static void	sort_until_appear_med(t_deques *deques);
-static void	divide_a_start(t_deques *deques);
+static void	sort_until_appear_med(t_div_info *div_info);
+static void	divide_a_start(t_div_info *div_info);
+static void	set_div_info(t_div_info *div_info);
 
 void	sort_a_start(t_deque *elements, t_deque *commands)
 {
-	t_deques	*deques;
+	t_div_info	*div_info;
 	size_t		size;
 
-	deques = create_deques(elements, commands);
-	free_and_setnull((void **)&(deques -> ab));
+	div_info = create_div_info(elements, commands);
 	size = elements -> size;
-	deques -> large = size - (size + 2) / 3;
-	deques -> small = (size + 1) / 3 - 1;
-	sort_until_appear_med(deques);
-	divide_a_start(deques);
-	delete_back_ra(deques);
-	sort_a_maxvalue(deques -> af, commands, deques -> large);
-	// sort_b_front(deques -> med_q, commands, deques -> small + 1, deques -> large - 1);
-	// sort_b_minvalue(deques -> small_q, commands, 0, deques -> small);
-	free(deques);
+	div_info -> large = size - (size + 2) / 3;
+	div_info -> small = size / 3 - 1;
+	sort_until_appear_med(div_info);
+	divide_a_start(div_info);
+	delete_back_ra(div_info);
+	div_info -> l_next(div_info -> l_q, div_info -> commands);
+	div_info -> m_next(div_info -> m_q, div_info -> commands);
+	div_info -> s_next(div_info -> s_q, div_info -> commands);
+	free(div_info);
 }
 
-static void	sort_until_appear_med(t_deques *deques)
+static void	sort_until_appear_med(t_div_info *div_info)
 {
 	int		number;
 	t_deque	*elements;
 
-	elements = deques -> elements;
+	elements = div_info -> elements;
 	while (elements -> size)
 	{
-		number = ((t_node *)(elements -> front))-> content.number;
-		if (number <= (deques -> small))
-			popfront_and_pushfront(deques, deques -> bb, pb);
-		else if (number >= (deques -> large))
-			popfront_and_pushback(deques, deques -> af, ra);
+		number = (elements -> front)-> content.number;
+		if (number <= (div_info -> small))
+			popfront_and_pushfront(div_info, div_info -> s_q, pb);
+		else if (number >= (div_info -> large))
+			popfront_and_pushback(div_info, div_info -> l_q, ra);
 		else
 			break ;
 	}
 }
 
-static void	divide_a_start(t_deques *deques)
+static void	divide_a_start(t_div_info *div_info)
 {
-	t_deque	*hold;
 	t_deque	*elements;
 	int		number;
 
-	hold = create_deque();
-	elements = deques -> elements;
+	set_div_info(div_info);
+	elements = div_info -> elements;
 	while (elements -> size)
 	{
-		number = ((t_node *)(elements -> front))-> content.number;
-		if (number <= (deques -> small))
-			popfront_and_pushfront(deques, hold, pb);
-		else if (number >= (deques -> large))
-			move_hold_one((t_hold_info){hold, deques -> bb, elements, \
-									deques -> af, deques -> commands, rb, ra});
+		number = elements -> front -> content.number;
+		if (number <= (div_info -> small))
+			div_info -> \
+				s_push(div_info, div_info -> s_q, div_info -> s_command);
+		else if (number >= (div_info -> large))
+			div_info -> \
+				l_push(div_info, div_info -> l_q, div_info -> l_command);
 		else
-		{
-			move_hold_all(hold, deques -> bb, deques -> commands, rb);
-			popfront_and_pushfront(deques, deques -> bf, pb);
-		}
+			div_info -> \
+				m_push(div_info, div_info -> m_q, div_info -> m_command);
 	}
+	move_hold_all(div_info, NULL, 0);
 	delete_deque(elements);
-	move_hold_all(hold, deques -> bb, deques -> commands, rb);
-	delete_deque(hold);
+	swap_hold(div_info);
+}
+
+static void	set_div_info(t_div_info *div_info)
+{
+	div_info -> hold = div_info -> s_q;
+	div_info -> hold_after = create_deque();
+	div_info -> l_command = ra;
+	div_info -> m_command = pb;
+	div_info -> s_command = pb;
+	div_info -> hold_command = rb;
+	div_info -> l_push = move_hold_one;
+	div_info -> m_push = move_hold_all;
+	div_info -> s_push = popfront_and_pushfront;
+	div_info -> l_next = sort_a_maxvalue;
+	div_info -> m_next = sort_b_front;
+	div_info -> s_next = sort_b_minvalue;
 }
