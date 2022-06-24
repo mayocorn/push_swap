ae5268b8dab357b482e18f7936276b9f8d05803a
mayocorn <stsunoda@student.42tokyo.jp>
2022-06-24 14:26:24 +0900

    style(push_swap.c): apply norm style

--- src/push_swap/push_swap.c
+++ src/push_swap/push_swap.c
@@ -6,13 +6,13 @@
 /*   By: mayocorn <twitter@mayocornsuki>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 20:45:42 by mayocorn          #+#    #+#             */
-/*   Updated: 2022/06/23 20:12:24 by mayocorn         ###   ########.fr       */
+/*   Updated: 2022/06/24 14:25:23 by mayocorn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/push_swap.h"
 
-static void	create_initial_deque(size_t size, int *array, t_deque *element_list);
+static void	create_initial_deque(size_t size, int *array, t_deque *elements);
 
 t_deque	*push_swap(size_t size, int **array)
 {
@@ -22,17 +22,16 @@ t_deque	*push_swap(size_t size, int **array)
 	commands = create_deque();
 	elements = create_deque();
 	create_initial_deque(size, *array, elements);
-	free_and_setnull((void**)array);
+	free_and_setnull((void **)array);
 	sort_a_start(elements, commands);
 	return (commands);
 }
 
 static void	create_initial_deque(size_t size, int *array, t_deque *elements)
 {
-	while(size--)
+	while (size--)
 	{
 		pushback(elements, create_number_node(*array));
 		array++;
 	}
 }
-
