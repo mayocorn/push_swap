cdc1a14a4b512349b4530c6f169d030296b7ff5d
mayocorn <stsunoda@student.42tokyo.jp>
2022-07-01 04:49:42 +0900

    refactor(array.h): apply norm style

--- include/array.h
+++ include/array.h
@@ -6,7 +6,7 @@
 /*   By: mayocorn <twitter@mayocornsuki>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 20:29:57 by mayocorn          #+#    #+#             */
-/*   Updated: 2022/06/21 15:21:30 by mayocorn         ###   ########.fr       */
+/*   Updated: 2022/07/01 04:48:55 by mayocorn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 # define POSITIVE 1
 # define NEGATIVE -1
 
-int	*duplicate_int_array(size_t size, int *src);
-void check_unique(size_t size, int *array);
-int	*convert_array_str_to_int(size_t array_size, char **src);
+int		*convert_array_str_to_int(size_t array_size, char **src);
+int		*duplicate_int_array(size_t size, int *src);
+void	check_unique(size_t size, int *array);
 #endif
