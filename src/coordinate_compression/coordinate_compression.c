679f7b3c59372db85b81f6886023a24836ffc7b9
mayocorn <stsunoda@student.42tokyo.jp>
2022-07-01 05:08:22 +0900

    refactor(coordinate_compression): apply norm style

--- src/coordinate_compression/coordinate_compression.c
+++ src/coordinate_compression/coordinate_compression.c
@@ -6,7 +6,7 @@
 /*   By: mayocorn <twitter@mayocornsuki>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 09:20:08 by mayocorn          #+#    #+#             */
-/*   Updated: 2022/06/23 20:18:27 by mayocorn         ###   ########.fr       */
+/*   Updated: 2022/07/01 05:05:55 by mayocorn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,8 @@ int	*coordinate_compression(size_t size, int **original, int **sorted)
 		res[i] = get_index(size, *sorted, (*original)[i]);
 		i++;
 	}
-	free_and_setnull((void**)sorted);
-	free_and_setnull((void**)original);
+	free_and_setnull((void **)sorted);
+	free_and_setnull((void **)original);
 	return (res);
 }
 
