848c4a3755095c70fa99e48a5b4ffbc77d8a7f81
mayocorn <stsunoda@student.42tokyo.jp>
2022-07-01 16:13:18 +0900

    refactor(optimize_pa): change static func

--- src/optimize/optimize_pa1.c
+++ src/optimize/optimize_pa1.c
@@ -6,7 +6,7 @@
 /*   By: mayocorn <twitter@mayocornsuki>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 13:26:02 by mayocorn          #+#    #+#             */
-/*   Updated: 2022/06/30 18:33:09 by mayocorn         ###   ########.fr       */
+/*   Updated: 2022/07/01 16:09:30 by mayocorn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ void	optimize_pa(t_deque *commands, t_node **node)
 {
 	t_node	*prev;
 
-	prev = (*node) -> prev;
+	prev = (*node)->prev;
 	if (prev == NULL)
-		*node = (*node) -> next;
+		*node = (*node)->next;
 	else if (prev -> content.command == pa)
 		return (optimize_sbpapa(commands, node));
 	else if (prev -> content.command == pb)
@@ -32,12 +32,8 @@ void	optimize_pa(t_deque *commands, t_node **node)
 		return (optimize_pbrapa(commands, node));
 	else if (prev -> content.command == rb)
 		return (optimize_sbrbpa(commands, node));
-	else if (prev -> content.command == rra)
-		return (optimize_pbrrapa(commands, node));
-	else if (prev -> content.command == sa)
-		return (optimize_rrapbsapa(commands, node));
 	else
-		*node = (*node) -> next;
+		return (optimize_pa_sub(commands, node));
 }
 
 static void	optimize_sbpapa(t_deque *commands, t_node **node)
@@ -45,24 +41,24 @@ static void	optimize_sbpapa(t_deque *commands, t_node **node)
 	t_node	*pa_node;
 	t_node	*sb_node;
 
-	pa_node = (*node) -> prev;
+	pa_node = (*node)->prev;
 	sb_node = pa_node -> prev;
 	if (sb_node != NULL && sb_node -> content.command == sb)
 	{
 		sb_node -> content.command = pa;
-		(*node) -> content.command = sa;
+		(*node)->content.command = sa;
 		*node = sb_node;
 	}
 	else
-		*node = (*node) -> next;
+		*node = (*node)->next;
 }
 
 static void	optimize_pbpa(t_deque *commands, t_node **node)
 {
 	t_node	*next;
 
-	next = (*node) -> next;
-	disjoint_node(commands, (*node) -> prev);
+	next = (*node)->next;
+	disjoint_node(commands, (*node)->prev);
 	disjoint_node(commands, *node);
 	*node = next;
 }
@@ -72,7 +68,7 @@ static void	optimize_pbrapa(t_deque *commands, t_node **node)
 	t_node	*ra_node;
 	t_node	*pb_node;
 
-	ra_node = (*node) -> prev;
+	ra_node = (*node)->prev;
 	pb_node = ra_node -> prev;
 	if (pb_node != NULL && pb_node -> content.command == pb)
 	{
@@ -81,7 +77,7 @@ static void	optimize_pbrapa(t_deque *commands, t_node **node)
 		*node = pb_node;
 	}
 	else
-		*node = (*node) -> next;
+		*node = (*node)->next;
 }
 
 void	optimize_sbrbpa(t_deque *commands, t_node **node)
@@ -89,7 +85,7 @@ void	optimize_sbrbpa(t_deque *commands, t_node **node)
 	t_node	*rb_node;
 	t_node	*sb_node;
 
-	rb_node = (*node) -> prev;
+	rb_node = (*node)->prev;
 	sb_node = rb_node -> prev;
 	if (sb_node != NULL && sb_node -> content.command == sb)
 	{
@@ -98,5 +94,5 @@ void	optimize_sbrbpa(t_deque *commands, t_node **node)
 		*node = sb_node;
 	}
 	else
-		*node = (*node) -> next;
+		*node = (*node)->next;
 }
--- src/optimize/optimize_pa2.c
+++ src/optimize/optimize_pa2.c
@@ -6,36 +6,52 @@
 /*   By: mayocorn <twitter@mayocornsuki>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 19:36:42 by mayocorn          #+#    #+#             */
-/*   Updated: 2022/06/28 19:48:26 by mayocorn         ###   ########.fr       */
+/*   Updated: 2022/07/01 16:11:55 by mayocorn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/optimize.h"
 
-void	optimize_pbrrapa(t_deque *commands, t_node **node)
+static void	optimize_pbrrapa(t_deque *commands, t_node **node);
+static void	optimize_rrapbsapa(t_deque *commands, t_node **node);
+
+void	oprimize_pa_sub(t_deque *commands, t_node **node)
+{
+	t_node	*prev;
+
+	prev = (*node)->prev;
+	if (prev -> content.command == rra)
+		return (optimize_pbrrapa(commands, node));
+	else if (prev -> content.command == sa)
+		return (optimize_rrapbsapa(commands, node));
+	else
+		*node = (*node)->next;
+}
+
+static void	optimize_pbrrapa(t_deque *commands, t_node **node)
 {
 	t_node	*rra_node;
 	t_node	*pb_node;
 
-	rra_node = (*node) -> prev;
+	rra_node = (*node)->prev;
 	pb_node = rra_node -> prev;
 	if (pb_node != NULL && pb_node -> content.command == pb)
 	{
 		disjoint_node(commands, pb_node);
-		(*node) -> content.command = sa;
+		(*node)->content.command = sa;
 		*node = rra_node;
 	}
 	else
-		*node = (*node) -> next;
+		*node = (*node)->next;
 }
 
-void	optimize_rrapbsapa(t_deque *commands, t_node **node)
+static void	optimize_rrapbsapa(t_deque *commands, t_node **node)
 {
-	t_node *sa_node;
-	t_node *pb_node;
-	t_node *rra_node;
+	t_node	*sa_node;
+	t_node	*pb_node;
+	t_node	*rra_node;
 
-	sa_node = (*node) -> prev;
+	sa_node = (*node)->prev;
 	pb_node = sa_node -> prev;
 	if (pb_node != NULL && pb_node -> content.command == pb)
 	{
@@ -44,10 +60,10 @@ void	optimize_rrapbsapa(t_deque *commands, t_node **node)
 		{
 			disjoint_node(commands, rra_node);
 			disjoint_node(commands, pb_node);
-			(*node) -> content.command = rra;
+			(*node)->content.command = rra;
 			*node = sa_node;
 			return ;
 		}
 	}
-	*node = (*node) -> next;
+	*node = (*node)->next;
 }
