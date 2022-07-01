8d4f0dc7455feff0f1f96923c6f83235b9431069
mayocorn <stsunoda@student.42tokyo.jp>
2022-07-01 17:03:17 +0900

    feat(main.h): change pushswap.h filename

--- include/main.h
+++ include/main.h
@@ -6,7 +6,7 @@
 /*   By: mayocorn <twitter@mayocornsuki>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/22 09:15:19 by mayocorn          #+#    #+#             */
-/*   Updated: 2022/06/25 18:44:08 by mayocorn         ###   ########.fr       */
+/*   Updated: 2022/07/01 15:54:17 by mayocorn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # include "deque.h"
 # include "mergesort.h"
 # include "coordinate_compression.h"
-# include "push_swap.h"
+# include "pushswap.h"
 # include "optimize.h"
 # include "console_out.h"
 #endif
